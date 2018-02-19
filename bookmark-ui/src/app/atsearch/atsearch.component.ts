7067a9f27b6d880b94ecea1d4ac741fad96cb78d
Junesh Kumar Subramani <juneshkumar.subramani@astrazeneca.com>
2018-02-19 17:09:19 +0530

    atsearch implemented

--- bookmark-ui/src/app/atsearch/atsearch.component.ts
+++ bookmark-ui/src/app/atsearch/atsearch.component.ts
@@ -5,6 +5,9 @@ import {Observable} from 'rxjs/Observable';
 import {startWith} from 'rxjs/operators/startWith';
 import {map} from 'rxjs/operators/map';
 
+import {MatAutocomplete, MatAutocompleteSelectedEvent, MatChipInputEvent} from '@angular/material';
+import {ENTER, COMMA} from '@angular/cdk/keycodes';
+
 
 
 export class State {
@@ -18,33 +21,32 @@ export class State {
 })
 export class AtsearchComponent {
 
+
+  txtInput: string;
+
+
+
   stateCtrl: FormControl;
   filteredStates: Observable<any[]>;
 
   states: State[] = [
     {
-      name: 'Arkansas',
+      name: 'Lemon',
       population: '2.978M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
     },
     {
-      name: 'California',
+      name: 'Lime',
       population: '39.14M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
     },
     {
-      name: 'Florida',
+      name: 'Apple',
       population: '20.27M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
-    },
-    {
-      name: 'Texas',
-      population: '27.47M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
     }
   ];
 
@@ -58,9 +60,74 @@ export class AtsearchComponent {
   }
 
   filterStates(name: string) {
+
     return this.states.filter(state =>
     state.name.toLowerCase().indexOf(name.toLowerCase()) === 0);
+
   }
+
+
+
+  //For chip
+  visible: boolean = true;
+  selectable: boolean = true;
+  removable: boolean = true;
+  addOnBlur: boolean = true;
+// Enter, comma
+  separatorKeysCodes = [ COMMA];
+
+  fruits = [
+
+  ];
+
+
+  add(event: MatChipInputEvent): void {
+    let input = event.input;
+    let value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  addStatus(event: MatAutocompleteSelectedEvent): void{
+
+   //console.log(event)
+   //this.stateCtrl.setValue(event.option.value);
+
+    //this.txtInput = 'ABC';
+
+  }
+
+  onKey(event: any) { // without type info
+    console.log(event.target.value);
+
+
+      if (event.keyCode == 13 && event.target.value.indexOf(':') != -1) {
+      this.fruits.push({name: event.target.value});
+      event.target.value='';
+    }
+
+  }
+
+
+
+  remove(fruit: any): void {
+    let index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+
+
 
 }
 
